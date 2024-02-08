65bc54ad90f7cf2e1cee6415869955edc64b3bb2
Mario Mata <mmatas1300@gmail.com>
2024-02-08 13:26:45 -0600

    add range and speed control

--- Servo/ServoAsync/ServoAsync.ino
+++ Servo/ServoAsync/ServoAsync.ino
@@ -7,7 +7,8 @@ int pinServo1 = 9;
 long positionServo1 = 1500;
 int directionServo1 = 0; // 0-left - 1-right
 unsigned long previousMillis1 = 0;
-unsigned long await1 = 1;//Definir rango
+unsigned long await1 = 1;//
+unsigned long range1 = 0;//
 
 //======Vars Global=====//
 unsigned long currentMillis = 0;
@@ -16,6 +17,7 @@ void setup()
 {
 	MyServo1.attach(pinServo1);
 	MyServo1.writeMicroseconds(1500); // set servo to mid-point
+  Serial.begin(9600);
 }
 
 void loop()
@@ -30,13 +32,23 @@ void loop()
 
 void trajectoryServo1()
 {
+
+
+  int potAwait = analogRead(A0); //lectura analógica
+
+  await1 = map(potAwait,0,1023,0,30); //función map para escalamiento
+
 	if (currentMillis - previousMillis1 > await1)
 	{
+    int potRange = analogRead(A1); //lectura analógica
+    range1 = map(potRange,0,1023,0,840); 
+    Serial.println(range1); //primera variable a mostrar
+
 		if (directionServo1 == 0)
 		{
 			positionServo1--;
 			MyServo1.writeMicroseconds(positionServo1);
-			if (positionServo1 == 700)
+			if (positionServo1 < (720+range1))
 			{
 				directionServo1 = 1;
 			}
@@ -46,7 +58,7 @@ void trajectoryServo1()
 		{
 			positionServo1++;
 			MyServo1.writeMicroseconds(positionServo1);
-			if (positionServo1 == 2400)
+			if (positionServo1 > (2380-range1))
 			{
 				directionServo1 = 0;
 			}
