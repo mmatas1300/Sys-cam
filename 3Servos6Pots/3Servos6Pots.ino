242ea7d704365df063c04fb7c4e09d5e90c579f5
Mario Mata <mmatas1300@gmail.com>
2024-02-13 19:07:22 -0600

    add servo3

--- 3Servos6Pots/3Servos6Pots.ino
+++ 3Servos6Pots/3Servos6Pots.ino
@@ -13,30 +13,52 @@ unsigned long range1 = 0;//
 //======Vars Servo 2=====//
 //======================//
 Servo MyServo2;
-int pinServo2 = 11;
+int pinServo2 = 10;
 long positionServo2 = 1500;
 int directionServo2 = 0; // 0-left - 1-right
 unsigned long previousMillis2 = 0;
 unsigned long await2 = 1;//
 unsigned long range2 = 0;//
+//=======================//
+//======Vars Servo 3=====//
+//======================//
+Servo MyServo3;
+int pinServo3 = 11;
+long positionServo3 = 1500;
+int directionServo3 = 0; // 0-left - 1-right
+unsigned long previousMillis3 = 0;
+unsigned long await3 = 1;//
+unsigned long range3 = 0;//
 
 //======Vars Global=====//
 unsigned long currentMillis = 0;
+const int buttonMode = 7;  // 
 
 void setup()
 {
+  pinMode(buttonMode, INPUT);
 	MyServo1.attach(pinServo1);
-	MyServo1.writeMicroseconds(1500); // set servo to mid-point
   MyServo2.attach(pinServo2);
-	MyServo2.writeMicroseconds(1500); // set servo to mid-point
+  MyServo3.attach(pinServo3);
+	MyServo1.writeMicroseconds(1500); // set servo to mid-point
+  MyServo2.writeMicroseconds(1500); // set servo to mid-point
+  MyServo3.writeMicroseconds(1500); // set servo to mid-point
 }
 
 void loop()
 {
-  currentMillis = millis();
-	trajectoryServo1();
-  currentMillis = millis();
-	trajectoryServo2();
+  mode = digitalRead(buttonMode);
+  if (buttonState == HIGH) {
+    currentMillis = millis();
+	  trajectoryServo1();
+    currentMillis = millis();
+	  trajectoryServo2();
+    currentMillis = millis();
+	  trajectoryServo3();
+  } else {
+
+  }
+
 }
 
 
@@ -87,7 +109,6 @@ void trajectoryServo1()
 void trajectoryServo2()
 {
   int potAwait = analogRead(A2); //lectura analógica
-
   await2 = map(potAwait,0,1023,0,30); //función map para escalamiento
 
 	if (currentMillis - previousMillis2 > await2)
@@ -118,5 +139,42 @@ void trajectoryServo2()
 	}
 }
 
+//=======================//
+//======Servo 3 rutina=====//
+//======================//
+
+void trajectoryServo3()
+{
+  int potAwait = analogRead(A4); 
+
+  await3 = map(potAwait,0,1023,0,30); 
+
+	if (currentMillis - previousMillis3 > await3)
+	{
+    int potRange = analogRead(A5); 
+    range3 = map(potRange,0,1023,0,840); 
+
+		if (directionServo3 == 0)
+		{
+			positionServo3--;
+			MyServo3.writeMicroseconds(positionServo3);
+			if (positionServo3 < (600+range3))
+			{
+				directionServo3 = 1;
+			}
+		}
+
+		if (directionServo3 == 1)
+		{
+			positionServo3++;
+			MyServo3.writeMicroseconds(positionServo3);
+			if (positionServo3 > (2300-range3))
+			{
+				directionServo3 = 0;
+			}
+		}
+		previousMillis3 = currentMillis;
+	}
+}
 
 
